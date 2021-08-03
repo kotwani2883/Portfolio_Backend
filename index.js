29fe1fbc93d7682dcbeadb2a0f273858822bd971
unknown <palakkotwani2883@gmail.com>
2021-08-04 01:04:52 +0530

    Initial Commit

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ app.post("/contact", (req, res) => {
     });
 });
 
-const port = process.env.PORT || '5000';
+const port = process.env.PORT || '3000';
 app.listen(port, () => {
     console.log(`Server is up and Running!!`);
 })
