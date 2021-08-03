9197d20caf9f3e91dd69a813aa376bc9d585847a
unknown <palakkotwani2883@gmail.com>
2021-08-04 00:57:41 +0530

    Made Changes

--- index.js
+++ index.js
@@ -55,6 +55,8 @@ app.post("/contact", (req, res) => {
         }
     });
 });
-app.listen(5000, () => {
+
+const port = process.env.PORT || '5000';
+app.listen(port, () => {
     console.log(`Server is up and Running!!`);
 })
